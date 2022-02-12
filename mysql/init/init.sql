0de67b24a24af6844378ceba3b66b30d1976eb96
hiro.0405soccer@icloud.com <hiro.0405soccer@icloud.com>
2022-02-12 23:58:58 +0900

    companiesテーブル追加

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -3,7 +3,8 @@ CREATE TABLE sample_table
   `id`         int(11) NOT NULL AUTO_INCREMENT comment 'テスト用カラムです',
   `sample`     text comment 'Testカラム',
   PRIMARY KEY (`id`)
-);
+)
+comment='テーブルのコメントです';
 
 INSERT INTO sample_table (sample) VALUES ("sample1");
 INSERT INTO sample_table (sample) VALUES ("sample2");
